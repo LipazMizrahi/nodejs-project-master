1bc8fe6b050800fd0ea380aabe1b5e5ef29444d6
LipazMizrahi <96912091+LipazMizrahi@users.noreply.github.com>
2023-06-21 12:55:53 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//Lipaz Mizrahi - 206505745
-//Maya Sasson - 209222769
-const app = require("./server");
-
-app.listen(process.env.PORT || 3000, "localhost", () => {
-  console.log(`Server up and running at ${process.env.PORT || 3000}`);
-});
-
-module.exports = app;
